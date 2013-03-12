31e865b71b66611fa5aee7f2b22f1249d44d90d1
Feng Li <feng.li@inria.fr>
2013-03-12 13:55:05 +0100

    Comment out memorization part.

--- src/compress_bsort2.c
+++ src/compress_bsort2.c
@@ -1823,7 +1823,7 @@ optimized_seq_sort ( UChar *block, Int32 last, Int32 *zptr, UInt16 *quadrant,
          The main sorting loop.
       --*/
 
-#pragma omp parallel for num_threads(8) shared (ftab, runningOrder, block, quadrant, bigDone, zptr, last) firstprivate (j,ss,sb)
+#pragma omp parallel for schedule (static) num_threads(24) default (none) shared (ftab, runningOrder, block, quadrant, bigDone, zptr, last, verbosity, stderr, numQSorted, firstAttempt_p, workLimit_p, workDone_p) private (j,ss,sb)
       for (i = 0; i <= 255; i++) {
 	{
 
@@ -1870,23 +1870,23 @@ optimized_seq_sort ( UChar *block, Int32 last, Int32 *zptr, UInt16 *quadrant,
          --*/
          bigDone[ss] = True;
 
-         if (i < 255) {
-            Int32 bbStart  = ftab[ss << 8] & CLEARMASK;
-            Int32 bbSize   = (ftab[(ss+1) << 8] & CLEARMASK) - bbStart;
-            Int32 shifts   = 0;
+         /* if (i < 255) { */
+         /*    Int32 bbStart  = ftab[ss << 8] & CLEARMASK; */
+         /*    Int32 bbSize   = (ftab[(ss+1) << 8] & CLEARMASK) - bbStart; */
+         /*    Int32 shifts   = 0; */
 
-            while ((bbSize >> shifts) > 65534) shifts++;
+         /*    while ((bbSize >> shifts) > 65534) shifts++; */
 
-            for (j = 0; j < bbSize; j++) {
-               Int32 a2update     = zptr[bbStart + j];
-               UInt16 qVal        = (UInt16)(j >> shifts);
-               quadrant[a2update] = qVal;
-               if (a2update < NUM_OVERSHOOT_BYTES)
-                  quadrant[a2update + last + 1] = qVal;
-            }
+         /*    for (j = 0; j < bbSize; j++) { */
+         /*       Int32 a2update     = zptr[bbStart + j]; */
+         /*       UInt16 qVal        = (UInt16)(j >> shifts); */
+         /*       quadrant[a2update] = qVal; */
+         /*       if (a2update < NUM_OVERSHOOT_BYTES) */
+         /*          quadrant[a2update + last + 1] = qVal; */
+         /*    } */
 
-            if (! ( ((bbSize-1) >> shifts) <= 65535 )) panic ( "sortIt" );
-         }
+         /*    if (! ( ((bbSize-1) >> shifts) <= 65535 )) panic ( "sortIt" ); */
+         /* } */
 
          /*--
             Now scan this big bucket so as to synthesise the
