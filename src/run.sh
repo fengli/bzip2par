9ce48a7d54e78fc678713cf3264ec28a8ea5d511
Feng Li <feng.li@inria.fr>
2013-06-04 14:20:24 +0200

    Cleanup.

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./obzip2 gdb.tar
+./bzip2 gdb.tar
 mv out.compress.7 out.compress.7.1
-./bbzip2 gdb.tar
+./pbzip2 gdb.tar
 echo "===>testing correctness by comparing zipped file"
 difference=`diff out.compress.7*`
 if ["$difference" -eq ""]
